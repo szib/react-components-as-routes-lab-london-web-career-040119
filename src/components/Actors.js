92c996f0420b4c63fb6240f0cf2a2131856df637
Ivan Szebenszki <szib77@gmail.com>
2019-06-11 10:29:09 +0100

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {
+        actors.map(actor => {
+          return (
+            <div key={actor.name}>
+              <p>{actor.name}</p>
+              <ul>
+                {actor.movies.map(movie => <li>{movie}</li>)}
+              </ul>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {
+          movies.map(movie => {
+            return (
+              <div key={movie.title}>
+                <p>{movie.title}</p>
+                <p>{movie.time}</p>
+                <p>{movie.metascore}</p>
+                <ul>
+                  {movie.genres.map(genre => <li>{genre}</li>)}
+                </ul>
+              </div>
+            )
+          })
+        }
     </div>
   );
 };
